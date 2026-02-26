f6eb3af9c701046e9744897c2d70f77851b5586d
Prajwal3219 <prajvalkoli3@gmail.com>
2026-02-26 17:32:03 +0530

    Soved SubArr problem

---
+++ LongestSubarraySumK.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class LongestSubarraySumK {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 5, 2, 7, 1, 9};
+        int k = 15;
+
+        int result = longestSubarray(arr, k);
+        System.out.println("Longest subarray length = " + result);
+    }
+
+    public static int longestSubarray(int[] arr, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            sum += arr[i];
+
+     
+            if (sum == k)
+                maxLen = i + 1;
+
+            
+            if (map.containsKey(sum - k)) {
+                int len = i - map.get(sum - k);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            // store first occurrence only
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
